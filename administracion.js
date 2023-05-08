cb698699a491164f30f8a629630f24127c90c10e
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-05-08 19:54:01 -0300

    edicion y modificaciones en el js sobre agregar/editar/eliminar prod

--- administracion.js
+++ administracion.js
@@ -4,7 +4,9 @@ const nombreDeProducto = document.getElementById(`nombre`)
 const precioDeProducto = document.getElementById(`precio`)
 const descripcionDeProducto = document.getElementById(`descripcion`)
 const codigoDeProducto = document.getElementById(`codigo`)
-const añadirBoton = document.getElementById(`Guardar`)
+const añadirBoton = document.getElementById(`enviarproducto`)
+const formulario = document.getElementById(`formularioproducto`)
+const tablaDeProd = Document.getElementById(`tablaDeProd`)
 
 añadirBoton.addEventListener(`click`, (e) => {
     e.preventDefault();
@@ -12,19 +14,10 @@ añadirBoton.addEventListener(`click`, (e) => {
     const nombre = nombreDeProducto.value
     const precio = precioDeProducto.value
     const descripcion = descripcionDeProducto.value
-    const codigo = codigoDeProducto.value
-
-
+    // const codigo = codigoDeProducto.value
+    const producto = { nombre, precio, descripcion };
+    productos.push(producto);
+    // console.log(productos);
+    localStorage.setItem(`productos`, JSON.stringify(productos));
+    formulario.reset();
 });
-
-const { v4: uuidv4 } = require('uuid');
-
-// Genera un código único de 5 dígitos utilizando la librería uuid
-function generateUniqueCode() {
-  var code = "#" + uuidv4().slice(0, 5);
-  return code;
-}
-
-// Llama a la función para generar el código
-var code = generateUniqueCode();
-console.log(code);
