a08afdcbeb7980c5911ea7a6f4cab5e7e77f4fa5
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-05-11 18:58:05 -0300

    ajusten en boton eliminar y agrego el boton editar

--- administracion.js
+++ administracion.js
@@ -2,6 +2,7 @@ let productos = []
 
 const listado = document.getElementById(`tablaDeProd`)
 const listadoEliminar = document.getElementById(`tablaDeProdEliminar`)
+const listadoEditar = document.getElementById(`tablaDeProdEditar`)
 const nombreDeProducto = document.getElementById(`nombre`)
 const precioDeProducto = document.getElementById(`precio`)
 const descripcionDeProducto = document.getElementById(`descripcion`)
@@ -9,6 +10,7 @@ const añadirBoton = document.getElementById(`enviarproducto`)
 const formulario = document.getElementById(`formularioproducto`)
 const tbodylistado = document.getElementById(`bodytexto`)
 const tbodylistadoEliminar = document.getElementById(`bodytextoEliminar`)
+const tbodylistadoEditar = document.getElementById(`bodytextoEditar`)
 
 //FUNCION AÑADIR
 añadirBoton.addEventListener(`click`, (e) => {
@@ -24,7 +26,8 @@ añadirBoton.addEventListener(`click`, (e) => {
     localStorage.setItem(`productos`, JSON.stringify(productos));
 
     mostrarProd();
-    mostrarProdEnEliminar()
+    mostrarProdEnEliminar();
+    mostrarProdEnEditar();
     formulario.reset();
 });
 
@@ -56,7 +59,7 @@ function mostrarProdEnEliminar() {
     <td>${producto.nombre}</td>
     <td>${producto.precio}</td>
     <td>${producto.descripcion}</td>
-    <td class="eliminar" data-id="${producto.codigo}"><button id="eliminarProdBtn"><i class="fa-sharp fa-regular fa-circle-xmark fa-2xs" style="color: #ff0000;"></i></button></td>`;
+    <td class="areaEliminar"><button id="eliminarProdBtn" class="eliminar" data-id="${producto.codigo}"><i class="fa-sharp fa-regular fa-circle-xmark fa-2xs" style="color: #ff0000;"></i></button></td>`;
     
     tbodylistadoEliminar.appendChild(tr);
 
@@ -85,11 +88,25 @@ listadoEliminar.addEventListener("click", (e) => {
       productos.splice(index, 1);
 
       mostrarProd();
-      mostrarProdEnEliminar()
+      mostrarProdEnEliminar();
+      mostrarProdEnEditar();
     }
   }
-
-
-
-
 });
+
+function mostrarProdEnEditar() {
+  listadoEditar.querySelector("tbody").innerHTML = ""
+
+  productos.forEach((producto) => {
+    const tr = document.createElement(`tr`);
+    tr.innerHTML = `
+    <td>#${producto.codigo}</td>
+    <td>${producto.nombre}</td>
+    <td>${producto.precio}</td>
+    <td>${producto.descripcion}</td>
+    <td><button id="eliminarProdBtn" class="eliminar" data-id="${producto.codigo}"><i class="fa-regular fa-pen-to-square"></i></button></td>`;
+    
+    tbodylistadoEditar.appendChild(tr);
+
+  });
+}
