73151d0d33c387d7b646ff825cbda394415b408c
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-05-13 18:43:25 -0300

    Cambios en el apartado EDITAR

--- administracion.js
+++ administracion.js
@@ -11,6 +11,12 @@ const formulario = document.getElementById(`formularioproducto`)
 const tbodylistado = document.getElementById(`bodytexto`)
 const tbodylistadoEliminar = document.getElementById(`bodytextoEliminar`)
 const tbodylistadoEditar = document.getElementById(`bodytextoEditar`)
+const formularioEditar = document.getElementById(`formularioEditar`)
+const urlImagen = document.getElementById(`urlImagen`)
+const nombreEditar = document.getElementById(`nombreEditar`)
+const precioEditar = document.getElementById(`precioEditar`)
+const descripcionEditar = document.getElementById(`descripcionEditar`)
+
 
 //FUNCION AÑADIR
 añadirBoton.addEventListener(`click`, (e) => {
@@ -97,6 +103,8 @@ listadoEliminar.addEventListener("click", (e) => {
   }
 });
 
+
+//Muestra en Editar
 function mostrarProdEnEditar() {
   listadoEditar.querySelector("tbody").innerHTML = ""
 
@@ -107,9 +115,38 @@ function mostrarProdEnEditar() {
     <td>${producto.nombre}</td>
     <td>${producto.precio}</td>
     <td>${producto.descripcion}</td>
-    <td><button id="eliminarProdBtn" class="eliminar" data-id="${producto.codigo}"><i class="fa-regular fa-pen-to-square"></i></button></td>`;
+    <td class="areaEditar"><button id="editarProdBtn" class="editar btn btn-outline-info" data-id="${producto.codigo}" data-bs-toggle="modal" data-bs-target="#editarFormModal" ><svg xmlns="http://www.w3.org/2000/svg" id="botoneditari" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+  </svg>Editar</button></td>`;
     
     tbodylistadoEditar.appendChild(tr);
 
   });
 }
+
+const editarproductoboton = document.getElementById(`editarproductoboton`)
+
+//EDITAR PRODUCTO
+listadoEditar.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    const producto = productos.find((producto) => producto.codigo === id);
+    if (producto) {
+      document.getElementById("nombreEditar").value = producto.nombre; // Seteamos el valor del input nombre
+      document.getElementById("precioEditar").value = producto.precio; // Seteamos el valor del input precio
+      document.getElementById("descripcionEditar").value = producto.descripcion; // Seteamos el valor del input descripcion
+      listadoEditar.dataset.editId = id; // Seteamos el id del producto a editar
+      editarproductoboton.addEventListener("click", (e) => {
+        const nombre = nombreEditar.value
+        const precio = precioEditar.value
+        const descripcion = descripcionEditar.value
+        const producto = { nombre, precio, descripcion };
+        productos.push(producto);
+        mostrarProd();
+        mostrarProdEnEliminar();
+        mostrarProdEnEditar();
+        formularioEditar.reset();
+      })
+    }
+    }
+);
