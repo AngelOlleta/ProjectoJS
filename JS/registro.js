e17ac984fb7e57df3d49a9c66fb214e5caf54459
Franco  Paz <franco.paz.712@gmail.com>
2023-05-15 20:32:03 -0300

    new push registroLogin

---
+++ JS/registro.js
@@ -0,0 +1,29 @@
+const formRegistro = document.querySelector("#formRegistro");
+formRegistro.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nombreCompleto = document.querySelector("#nombreCompleto").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const nombreUsuario = document.querySelector("#nombreUsuario").value;
+  const telefono = document.querySelector("#telefono");
+  const domicilio = document.querySelector("#domicilio").value;
+  const codigoPostal = document.querySelector("#codigoPostal").value;
+
+  const Users = JSON.parse(localStorage.getItem("users")) || [];
+  const isUserRegistred = Users.find((user) => user.email === email);
+  if (isUserRegistred) {
+    return alert("el usuario ya esta registrado");
+  }
+  Users.push({
+    name: nombreCompleto,
+    email: email,
+    nickName: nombreUsuario,
+    password: password,
+    telefono: telefono,
+    domicilio: domicilio,
+    codigoPostal: codigoPostal,
+  });
+  localStorage.setItem("users", JSON.stringify(Users));
+  alert("Registro existoso! ");
+  window.location.href = "login.html";
+});
