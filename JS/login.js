6c9fa070db6a70877312c01c94f9a091b9bd6d75
Franco  Paz <franco.paz.712@gmail.com>
2023-05-15 17:16:22 -0300

    actualizacion iconos navbar

--- JS/login.js
+++ JS/login.js
@@ -1,19 +1,29 @@
+let userAdmin = {
+  name: "admin",
+  email: "admin@gmail.com",
+  password: "administrador",
+};
+let sesionIniciada = false;
 const loginForm = document.querySelector("#loginForm");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const userAdmin = {
-    nombreUsuario: "admin",
-    email: "admin@gmail.com",
-    password: "administrador",
-  };
+
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   const Users = JSON.parse(localStorage.getItem("users")) || [];
   const validarUsuario = Users.find(
     (user) => user.email === email && user.password === password
   );
-
-  if (!validarUsuario) {
+  if (email === userAdmin.email) {
+    if (password === userAdmin.password) {
+      sesionIniciada = true;
+      localStorage.setItem("usuario_logueado", JSON.stringify(sesionIniciada));
+      localStorage.setItem("user", JSON.stringify(userAdmin));
+      window.location.replace("index.html");
+    } else {
+      alert("Mail y/o contraseña incorrectos");
+    }
+  } else if (!validarUsuario) {
     alert("Mail y/o contraseña incorrectos! ");
   }
   alert(`Bienvenido ${validarUsuario.name}`);
