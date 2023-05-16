84170712dbec51a35eef1a75f1144742ed646af8
Franco  Paz <franco.paz.712@gmail.com>
2023-05-16 01:55:17 -0300

    se arreglan errores generales

--- JS/productosDeseados.js
+++ JS/productosDeseados.js
@@ -1,8 +1,48 @@
+let userAdmin = {
+  name: "admin",
+  email: "admin@gmail.com",
+  password: "administrador",
+};
 const productoFavoritoLS = JSON.parse(localStorage.getItem("favoritos")) || [];
+let usuarioLogueado =
+  JSON.parse(localStorage.getItem("usuario_logueado")) || false;
+let user = JSON.parse(localStorage.getItem("user")) || "";
+
 const cards = document.querySelector(".card-producto");
 // let favsProductos = JSON.parse(localStorage.getItem("favoritos")) || [];
 const contFav = document.querySelector(".contFav");
 contFav.innerHTML = productoFavoritoLS.length;
+const cerrarSesion = () => {
+  if (usuarioLogueado) {
+    usuarioLogueado = false;
+    localStorage.setItem("usuario_logueado", JSON.stringify(usuarioLogueado));
+    localStorage.removeItem("user");
+    localStorage.removeItem("usuario_logueado");
+    localStorage.removeItem("favoritos");
+    window.location.replace("index.html");
+  } else {
+    window.location.reload();
+  }
+};
+if (usuarioLogueado) {
+  if (user.name === userAdmin.name) {
+    linkAdmin.className = "nav-item";
+
+    btnDeslogueo.className = "";
+    contFav.className = "d-none";
+    btnLogueo.innerHTML = user.name;
+    btnLogueo.className = "px-3 nav-link active ";
+    btnLogueo.removeAttribute("href");
+    btnDeslogueo.addEventListener("click", cerrarSesion);
+  } else {
+    btnRegistro.className = "d-none";
+    btnDeslogueo.className = " px-1 ";
+    btnLogueo.innerHTML = usuarioLogueado.nickName;
+    btnLogueo.className = "px-3 nav-link active ";
+    btnLogueo.removeAttribute("href");
+    btnDeslogueo.addEventListener("click", cerrarSesion);
+  }
+}
 
 productoFavoritoLS.map((info) => {
   cards.innerHTML += `
